4b9b208318a5bb9f487ca5b98c7ca370ab5d4bed
Dj4rn0 <d.eijkenboom@sintlucasedu.nl>
2023-02-04 21:55:21 +0100

    InventoryPickup system + Inventory method additions to game manager script

---
+++ Malagajam/Assets/Scenes/Personal scenes/Djarno/Scripts/ItemPickupScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ItemPickupScript : MonoBehaviour
+{
+
+    [SerializeField] private float spinspeed = 0.5f;
+    [SerializeField] private string _itemName;
+    // Update is called once per frame
+    void Update()
+    {
+        //Makes the item spin
+        transform.Rotate(0f, spinspeed, 0f, Space.Self);
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+            GameManager.Instance.AddItemToList(_itemName);
+        }
+    }
+}
